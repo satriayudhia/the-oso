9421c966e5eb4a190bdae8b49f3a3959bf3b82b9
satriayudhia <satria.yudhiap@gmail.com>
2023-09-20 14:54:56 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2a-view-perumahan",
+      "name": "2A. VIEW PERUMAHAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.021184990353266,
+        "pitch": -0.06185111377130248,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4937671681662223,
+          "pitch": 0.0046704784434652424,
+          "rotation": 0,
+          "target": "1-1a-ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1a-ruang-utama",
+      "name": "1A. RUANG UTAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.9764011737001885,
+        "pitch": 0.10850250958265306,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4888891375746578,
+          "pitch": 0.41247370347176116,
+          "rotation": 0,
+          "target": "0-2a-view-perumahan"
+        },
+        {
+          "yaw": -2.466786895363022,
+          "pitch": 0.07545947631335537,
+          "rotation": 1.5707963267948966,
+          "target": "4-1d-kmr-mandi"
+        },
+        {
+          "yaw": 1.843402016531373,
+          "pitch": 0.17957774004883476,
+          "rotation": 0,
+          "target": "2-1b-ruang-dalam-1"
+        },
+        {
+          "yaw": 3.073864816182205,
+          "pitch": 0.13316916071659257,
+          "rotation": 0,
+          "target": "5-1e-halaman-blkng"
+        },
+        {
+          "yaw": 3.095589439619494,
+          "pitch": 0.4017419871886556,
+          "rotation": 1.5707963267948966,
+          "target": "3-1c-ruang-dalam-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1b-ruang-dalam-1",
+      "name": "1B. RUANG DALAM 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8117018772193383,
+        "pitch": 0.09696057228399546,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2456726690248674,
+          "pitch": 0.2948389876093387,
+          "rotation": 0,
+          "target": "1-1a-ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1c-ruang-dalam-2",
+      "name": "1C. RUANG DALAM 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0738372720920353,
+        "pitch": 0.20485241632993478,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9668311825841842,
+          "pitch": 0.23974452176538108,
+          "rotation": 0,
+          "target": "1-1a-ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-1d-kmr-mandi",
+      "name": "1D. KMR MANDI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.24628535000364238,
+        "pitch": 0.5573141002529312,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.719433854030534,
+          "pitch": 0.29945869711729856,
+          "rotation": 0,
+          "target": "1-1a-ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-1e-halaman-blkng",
+      "name": "1E. HALAMAN BLKNG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.1287784191011454,
+        "pitch": 0.04452322180311974,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.378420259062489,
+          "pitch": 0.14696069698658576,
+          "rotation": 0,
+          "target": "1-1a-ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "THE OSO",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
